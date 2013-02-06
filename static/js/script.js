6b7dbd357e7f7f3d731ee1093d4ac9f2bda10c9b
Yoonjo Choi <yoonjochoi@Yoonjos-MacBook-Pro.local>
2013-02-05 23:39:33 -0500

    add profile

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,27 @@
 init = function(){
-	alert('here!');
-
 	var jarallax = new Jarallax();
-	jarallax.addAnimation('#header', 
+	jarallax.addAnimation('#yoonjoIs', 
 						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '60%'},
+						   {progress:'100%', top: '60%'}]);
+	
+	jarallax.addAnimation('#topBanner', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'20%', top: '100%'},
+						   {progress:'40%', top: '0%'},
 						   {progress:'100%', top: '0%'}]);
+						   
+	jarallax.addAnimation('#thumbnailsContainer', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'20%', top: '100%'},
+						   {progress:'40%', top: '0%'},
+						   {progress:'100%', top: '0%'}]);
+	
+	jarallax.addAnimation('#bottom', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'80%', top: '100%'},
+						   {progress:'100%', top: '0%'}]);
+
+
 }
 
-init();
