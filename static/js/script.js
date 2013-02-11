e6086444b0ea05436f87356c6d20525f4ebdd490
Yoonjo Choi <yoonjochoi@Yoonjos-MacBook-Pro.local>
2013-02-11 00:58:15 -0500

    added thumbnails

--- static/js/script.js
+++ static/js/script.js
@@ -2,25 +2,70 @@ init = function(){
 	var jarallax = new Jarallax();
 	jarallax.addAnimation('#yoonjoIs', 
 						   [{progress:'0%', top: '100%'},
-						   {progress:'10%', top: '60%'},
+						   {progress:'5%', top: '60%'},
 						   {progress:'100%', top: '60%'}]);
+	
+	jarallax.addAnimation('#middle', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '100%'},
+						   {progress:'20%', top: '0%'},
+						   {progress:'100%', top: '0%'}]);
 	
 	jarallax.addAnimation('#topBanner', 
 						   [{progress:'0%', top: '100%'},
-						   {progress:'20%', top: '100%'},
-						   {progress:'40%', top: '0%'},
+						   {progress:'10%', top: '100%'},
+						   {progress:'20%', top: '0%'},
 						   {progress:'100%', top: '0%'}]);
 						   
-	jarallax.addAnimation('#thumbnailsContainer', 
-						   [{progress:'0%', top: '100%'},
-						   {progress:'20%', top: '100%'},
-						   {progress:'40%', top: '0%'},
-						   {progress:'100%', top: '0%'}]);
+	jarallax.addAnimation('#row1', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'25%', top: '100%', opacity: '1'},
+						   {progress:'35%', top: '17%', opacity: '1'},
+						   {progress:'100%', top: '17%', opacity: '0'}]);
+
+	jarallax.addAnimation('#row2', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'35%', top: '100%', opacity: '1'},
+						   {progress:'45%', top: '17%', opacity: '1'},
+						   {progress:'100%', top: '17%', opacity: '0'}]);
+					
+	jarallax.addAnimation('#row3', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'45%', top: '100%', opacity: '1'},
+						   {progress:'55%', top: '17%', opacity: '1'},
+						   {progress:'100%', top: '17%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#row4', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'55%', top: '100%', opacity: '1'},
+						   {progress:'65%', top: '17%', opacity: '1'},
+						   {progress:'100%', top: '17%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#row5', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'65%', top: '100%', opacity: '1'},
+						   {progress:'80%', top: '17%', opacity: '1'},
+						   {progress:'92%',opacity: '1'},
+						   {progress:'100%', top: '17%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#row6', 
+						   [{progress:'0%', top: '100%', opacity: '0'},
+						   {progress:'75%', top: '100%', opacity: '1'},
+						   {progress:'90%', top: '58.4%', opacity: '1'},
+						   {progress:'92%',opacity: '1'},
+						   {progress:'100%', top: '58.4%', opacity: '0'}]);
 	
 	jarallax.addAnimation('#bottom', 
 						   [{progress:'0%', top: '100%'},
-						   {progress:'80%', top: '100%'},
+						   {progress:'96%', top: '100%'},
 						   {progress:'100%', top: '0%'}]);
+						   
+	jarallax.addAnimation('#wrapBio', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'96%', top: '100%'},
+						   {progress:'100%', top: '30%'}]);
+						   
+						   
 
 
 }
